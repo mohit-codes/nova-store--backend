9ddaea5f5cb52db1425bf6282af4cedc36ef81db
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-10 11:12:00 +0530

    refactor: product model update

--- models/product.model.js
+++ models/product.model.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema(
   {
     name: { type: mongoose.Schema.Types.String, required: true },
+    description: { type: mongoose.Schema.Types.String, required: true },
+    category: { type: mongoose.Schema.Types.String, required: true },
     price: { type: mongoose.Schema.Types.Number, required: true },
-    image: { type: mongoose.Schema.Types.String, required: true },
+    image: { type: mongoose.Schema.Types.Array, required: true },
     brand: { type: mongoose.Schema.Types.String, required: true },
-    details: { type: mongoose.Schema.Types.Array, required: true },
+    about: { type: mongoose.Schema.Types.Array, required: true },
     rating: { type: mongoose.Schema.Types.Number, required: true },
     isInStock: { type: mongoose.Schema.Types.Boolean, required: true },
     freeShipping: { type: mongoose.Schema.Types.Boolean, required: true },
