33813956f4699bc7ec07e69fc19a1382cbc94bcf
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-22 18:46:52 +0530

    feat: adds new routes for featured product and categories

--- routers/product.router.js
+++ routers/product.router.js
@@ -3,9 +3,13 @@ const router = Router();
 const {
   fetchAllProducts,
   fetchProductById,
+  fetchFeaturedProducts,
+  fetchProductCategories,
 } = require("../controllers/product.controller");
 
 router.route("/").get(fetchAllProducts);
-router.route("/:productId").get(fetchProductById);
+router.route("/single-product/:productId").get(fetchProductById);
+router.route("/featured").get(fetchFeaturedProducts);
+router.route("/categories").get(fetchProductCategories);
 
 module.exports = router;
