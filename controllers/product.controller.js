e0328b6837ec86f43dfae745c5563e7443129dd6
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-22 18:46:17 +0530

    feat: controllers for featured product and category fetching

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,6 @@
 const Product = require("../models/product.model");
+const FeaturedProduct = require("../models/featuredProducts.model");
+const Category = require("../models/category.model");
 
 const fetchAllProducts = async (_, res) => {
   try {
@@ -40,4 +42,41 @@ const fetchProductById = async (req, res) => {
   }
 };
 
-module.exports = { fetchAllProducts, fetchProductById };
+const fetchFeaturedProducts = async (_, res) => {
+  try {
+    const products = await FeaturedProduct.find({});
+    return res.json({
+      success: true,
+      products: products,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      products: null,
+      message: error.message,
+    });
+  }
+};
+
+const fetchProductCategories = async (_, res) => {
+  try {
+    const categories = await Category.find({});
+    return res.json({
+      success: true,
+      categories: categories,
+    });
+  } catch (error) {
+    return res.json({
+      success: false,
+      categories: null,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  fetchAllProducts,
+  fetchProductById,
+  fetchProductCategories,
+  fetchFeaturedProducts,
+};
