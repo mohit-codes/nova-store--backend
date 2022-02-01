70cbcbe7cd3c8063dbab69cdd7746035846a1182
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-01 19:33:16 +0530

    feat: added route to fetch user data

--- routers/user.router.js
+++ routers/user.router.js
@@ -3,9 +3,11 @@ const router = Router();
 const {
   loginUserAndSendCredentials,
   signupUserAndSendCredentials,
+  fetchUserDataById,
 } = require("../controllers/user.controller");
 
 router.route("/login").post(loginUserAndSendCredentials);
 router.route("/signup").post(signupUserAndSendCredentials);
+router.route("/user-data/:userId").get(fetchUserDataById);
 
 module.exports = router;
