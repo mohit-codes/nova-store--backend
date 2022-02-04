d3f15f9449733e3ea69a24ba1a48dc7974c1d7a3
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-04 19:44:49 +0530

    feat: added routers

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 const port = process.env.PORT || 3000;
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const userRouter = require("./routers/user.router");
 const cartRouter = require("./routers/cart.router");
 const wishlistRouter = require("./routers/wishlist.router");
 const productRouter = require("./routers/product.router");
+const addressRouter = require("./routers/address.router");
+const paymentRouter = require("./routers/payment.router");
+const authenticate = require("./middleware/authenticate");
 const { initializeDBConnection } = require("./config/db.config");
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 initializeDBConnection();
 
@@ -18,8 +23,10 @@ app.get("/", (res) => {
 
 app.use("/users", userRouter);
 app.use("/products", productRouter);
-app.use("/carts", cartRouter);
-app.use("/wishlists", wishlistRouter);
+app.use("/carts", authenticate, cartRouter);
+app.use("/wishlists", authenticate, wishlistRouter);
+app.use("/addresses", authenticate, addressRouter);
+app.use("/payment", authenticate, paymentRouter);
 
 app.listen(port, () => {
   console.log(`backend server running on port ${port}`);
